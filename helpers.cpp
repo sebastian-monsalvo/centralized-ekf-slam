9ef172e96a1461dc0da5931aad7aeba16e430d1a
Sebastian <monsalvo@stanford.edu>
2026-08-18 12:17:12 -0400

    finally did draft of f fn

--- helpers.cpp
+++ helpers.cpp
@@ -1,6 +1,25 @@
 #include <array>
+#include <iostream>
+#include <cmath>
 
-int main(){return 0;}
+std::array <float, 3> f(std::array <float, 3> mu, std::array <float, 2> u, float dt) {
+    std::array <float, 3> result = {0, 0, 0};
+    result[0] = mu[0] + dt * u[0] * std::cos(mu[2]); //angles are in rad already. no need to convert
+    result[1] = mu[1] + dt * u[0] * std::sin(mu[2]);
+    result[2] = mu[2] + dt * u[1];
 
-std::array <float, 4> my_array = {2.0, 3.0, 1.5, 4.4};
+    return result;
+}
+
+int main(){
+    std::array <float, 3> mu = {0, 0, 0};
+    std::array<float, 2> u = {3.3, 5.2};
+    mu = f(mu, u, 0.1);
+    std::cout << "x changed to: " << mu[0] << "\n";
+    std::cout << "the angular velocity is: " << u[1];
+    return 0;
+}
+
+std::array <float, 3> mu = {0.0, 0.0, 0.0};
+std::array <float, 2> u = {0.0, 0.0};
 
