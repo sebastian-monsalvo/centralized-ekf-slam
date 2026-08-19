0bae09a41739ecb42504a8b9313d0829793ae6bb
Sebastian <monsalvo@stanford.edu>
2026-08-19 09:45:54 -0400

    accidentally edited on main lol

--- ekf.cpp
+++ ekf.cpp
@@ -4,10 +4,12 @@
 
 
 int main() {
-    std::cout << "Hello, World!";
+    std::cout << "Hello, World! \n";
     std::array <float, 3> mu = {0, 0, 0};
-    std::array<float, 2> u = {3.3, 5.2};
+    std::array <float, 2> u = {3.3, 0.0};
     mu = f(mu, u, 0.1);
-    std::cout << "main function ran successfully";
+    std::cout << "main function ran successfully \n";
+    std::cout << "The new position of the robot is: \n";
+    std::cout << "x: " << mu[0] << "\ny: " << mu[1] << "\nth: " << mu[2];
     return 0;
 }
--- helpers.cpp
+++ helpers.cpp
@@ -3,7 +3,7 @@
 #include <cmath>
 
 std::array <float, 3> f(std::array <float, 3> mu, std::array <float, 2> u, float dt) {
-    std::array <float, 3> result = {0, 0, 0};
+    std::array <float, 3> result = {0.0, 0.0, 0.0};
     result[0] = mu[0] + dt * u[0] * std::cos(mu[2]); //angles are in rad already. no need to convert
     result[1] = mu[1] + dt * u[0] * std::sin(mu[2]);
     result[2] = mu[2] + dt * u[1];
